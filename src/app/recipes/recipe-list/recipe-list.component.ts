039a1b800e9d9ac36067b2b7ffda5a047d06f52b
Johann Peters <johann.peters@check24.de>
2019-05-08 10:55:10 +0200

    recipes sorted

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from'../recipe.model'
+import {Component, Input, OnInit, OnView, Output} from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  recipes: Recipe[]=[
+export class RecipeListComponent implements OnInit, OnView {
+  @Output() recipes: Recipe[]=[
     new Recipe('A Test Recipe',
         'This is a test recipe and this the description',
         'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
     new Recipe('A Second Recipe',
-      'This is a second test recipe and this the description',
-      'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
-];
-  constructor() { }
+        'This is a second test recipe and this the description',
+        'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
